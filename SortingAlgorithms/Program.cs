2aee2c43df60f0da2010416c964062c0305bdda5
DivideBySara <email address>
2018-08-26 15:05:15 -0700

    Added driving program for QuickSort

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -10,8 +10,25 @@ namespace SortingAlgorithms
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 1. QuickSort
+            Console.WriteLine("1. QuickSort");
+
+            int[] arr = { 3, 4, 1, 2 };
+            Console.WriteLine("\nStarting array: ");
+            foreach (int i in arr)
+            {
+                Console.Write(i + " ");
+            }
+
+            arr = QuickSort(arr, 0, arr.Length - 1);
+            Console.WriteLine("\n\nEnding array: ");
+
             Console.ReadKey();
+        }
+
+        private static int[] QuickSort(int[] arr, int leftIndex, int rightIndex)
+        {
+            throw new NotImplementedException();
         }
     }
 }
