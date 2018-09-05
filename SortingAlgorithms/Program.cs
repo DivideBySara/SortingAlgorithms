722acf0fd985a1d4fc861f93fe7f8c7eb22a9b0b
DivideBySara <email address>
2018-09-04 23:15:18 -0700

    I finally get QuickSort!

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -13,53 +13,82 @@ namespace SortingAlgorithms
             // 1. QuickSort
             Console.WriteLine("1. QuickSort");
 
-            int[] arr = { 3, 4, 1, 2 };
-            Console.WriteLine($"\nStarting array: {string.Join(" ", arr)}");
+            int[] arr1 = { 3, 4, 1, 2 };
+            Console.WriteLine($"\nStarting array: {string.Join(" ", arr1)}");
+            QuickSort(arr1, 0, arr1.Length - 1);
+            Console.WriteLine($"Ending array: {string.Join(" ", arr1)}");
 
-            arr = QuickSort(arr, 0, arr.Length - 1);
-            Console.WriteLine($"\nEnding array: {string.Join(" ", arr)}");
+            int[] arr2 = { 3 };
+            Console.WriteLine($"\nStarting array: {string.Join(" ", arr2)}");
+            QuickSort(arr2, 0, arr2.Length - 1);
+            Console.WriteLine($"Ending array: {string.Join(" ", arr2)}");
+
+            int[] arr3 = { 3, 4, 10, 9, 1, 7, 8 };
+            Console.WriteLine($"\nStarting array: {string.Join(" ", arr3)}");
+            QuickSort(arr3, 0, arr3.Length - 1);
+            Console.WriteLine($"Ending array: {string.Join(" ", arr3)}");
+
+            int[] arr4 = { 6, 3, 1, 2 };
+            Console.WriteLine($"\nStarting array: {string.Join(" ", arr4)}");
+            QuickSort(arr4, 0, arr4.Length - 1);
+            Console.WriteLine($"Ending array: {string.Join(" ", arr4)}");
+
+            arr4 = null;
+            Console.WriteLine("\nStarting array is null.");
+            QuickSort(arr4, 0, 0);
+            Console.WriteLine($"Ending array: {arr4}");
 
             Console.ReadKey();
         }
 
-        private static int[] QuickSort(int[] arr, int leftIndex, int rightIndex)
+        private static void QuickSort(int[] arr, int startIndex, int endIndex)
         {
-            if (leftIndex < rightIndex)
+            // Check for null
+            if (arr == null)
             {
-                int partition = Partition(arr, leftIndex, rightIndex);
-
-                arr = QuickSort(arr, 0, rightIndex);
-                arr = QuickSort(arr, rightIndex + 1, arr.Length - 1);
+                return;
             }
 
-            return arr;
-        }
-
-        private static int Partition(int[] arr, int leftIndex, int rightIndex)
-        {
-            // Choose a pivot value
-            int pivotValue = arr[leftIndex];
-            int small = leftIndex - 1;
-
-            for (int i = leftIndex; i < rightIndex; ++i)
+            // Base case
+            if (endIndex <= startIndex)
             {
-                if (arr[i] < pivotValue)
+                return;
+            }
+
+            // Things we need
+            int currentIndex = startIndex;
+            int pivotWallIndex = startIndex;
+            int pivotValue = arr[endIndex];
+
+            // Partition
+            while (currentIndex != endIndex)
+            {
+                // Compare currentIndex to pivotValue
+                if (arr[currentIndex] <= pivotValue)
                 {
-                    ++small;
-                    Swap(arr, i, small);
-                }
+                    Swap(arr, pivotWallIndex, currentIndex);
+                    ++pivotWallIndex;
+                } 
+                // else currentIndex is greater than pivotValue so do nothing
+
+                ++currentIndex;
             }
 
-            Swap(arr, rightIndex, small + 1);
+            // Swap pivotWall for pivot
+            Swap(arr, pivotWallIndex, endIndex);
 
-            return small + 1;
+            // Recurse left
+            QuickSort(arr, startIndex, pivotWallIndex - 1);
+
+            // Recurse right
+            QuickSort(arr, pivotWallIndex + 1, endIndex);
         }
 
         private static void Swap(int[] arr, int leftIndex, int rightIndex)
         {
             int temp = arr[leftIndex];
             arr[leftIndex] = arr[rightIndex];
-            arr[rightIndex] = arr[leftIndex];
+            arr[rightIndex] = temp;
         }
     }
 }
